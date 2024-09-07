5c6ee0782f9e8b054eeb8986e4c10986a7de15a2
hp <rajputvirendra@303>
2024-09-08 00:44:19 +0530

    Add file airplane

---
+++ src/ariplane/Airplane.java
@@ -0,0 +1,13 @@
+package ariplane;
+
+// Write a Java program to create a class called "Airplane" with a flight number , destination, and departure time attributes, and methods to check flight status and delay.
+
+import java.time.LocalDate;
+
+public class Airplane {
+   private String flightNumber;
+   private String destination;
+   private LocalDate departureTime;
+
+   
+}
